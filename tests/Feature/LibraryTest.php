fc10173eed38a9192a9d82766b4f0b632f21c66b
MSalah73 <ms24@pdx.edu>
2020-02-16 21:57:14 +0300

    Added userAbleToAddABook test

--- tests/Feature/LibraryTest.php
+++ tests/Feature/LibraryTest.php
@@ -19,12 +19,24 @@ class LibraryTest extends TestCase
     public function databaseAbleToAddABookTest()
     {
         $book = factory(Book::class)->create();
-        
+
         $this->assertDatabaseHas('books', [
             'id' => $book->id
         ]);
     }
-    public function userAbleToAddABookTest(){
+    public function userAbleToAddABookTest()
+    {
+        // This is a store action.
+        $response = $this->post('/book', [
+            'title' => 'Learning Laravel',
+            'author' => 'Mohammed Salah',
+        ]);
 
+        $response->assertStatus(201);
+
+        $this->assertDatabaseHas('books', [
+            'title' => 'Learning Laravel',
+            'author' => 'Mohammed Salah',
+        ]);
     }
 }
